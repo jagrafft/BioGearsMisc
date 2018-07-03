116e67f751d0e1aa2171dd7970f22c328b96ad3a
jason a. grafft <graf0096@umn.edu>
2018-07-03 14:18:08 -0500

    Replaced printable_scenario_x_m_l_file.html with PDF.

--- XMLBuilder.jl
+++ XMLBuilder.jl
@@ -1,7 +1,7 @@
 # using Graft
-using Lazy, LightXML
+# using Lazy, LightXML
 
-struct BGScenario
+mutable struct BGScenario
     document::LightXML.XMLDocument
     root::LightXML.XMLElement               # Scenario
     init::Array{LightXML.XMLElement, 1}   # InitialParameters || EngineStateFile
@@ -9,42 +9,61 @@ struct BGScenario
     description::LightXML.XMLElement
     dataRequests::Array{LightXML.XMLElement, 1}
     actions::Array{LightXML.XMLElement, 1}
+    BGScenario(r) = new(XMLDocument(), r)
 end
 
-scenario_attr = ["xmlns" => "uri:/mil/tatrc/physiology/datamodel", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "xsdVersion" => "v16.12", "contentVersion" => "BioGears_6.0.1-beta", "xsi:schemaLocation" => ""]
+data_requests = [
+        ["xsi:type" => "PhysiologyDataRequestData", "Name" => "HeartRate", "Unit" => "1/min"],
+        ["xsi:type" => "PhysiologyDataRequestData", "Name" => "MeanArterialPressure", "Unit" => "mmHg"],
+        ["xsi:type" => "PhysiologyDataRequestData", "Name" => "OxygenSaturation"]
+    ]
 
-drq_attr_hr = ["xsi:type" => "PhysiologyDataRequestData", "Name" => "HeartRate", "Unit" => "1/min"]
-drq_attr_map = ["xsi:type" => "PhysiologyDataRequestData", "Name" => "MeanArterialPressure", "Unit" => "mmHg"]
-drq_attr_spo2 = ["xsi:type" => "PhysiologyDataRequestData", "Name" => "OxygenSaturation"]
+function newbgs()
+    scenario_attr = [
+        "xmlns" => "uri:/mil/tatrc/physiology/datamodel",
+        "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
+        "xsdVersion" => "v16.12",
+        "contentVersion" => "BioGears_6.0.1-beta",
+        "xsi:schemaLocation" => ""
+    ]
+
+    sc = @> "Scenario" ne
+    @> sc set_attributes(scenario_attr)
+    BGScenario(sc)
+end
 
 ne(e::String) = @> e new_element
+chldtxt(r::LightXML.XMLElement, c::String, t::String) = @> r new_child(c) add_text(t)
 
 x = XMLDocument()
-sc = @> "Scenario" new_element
-set_attributes(sc, scenario_attr)
-set_root(x, sc)
-@> sc new_child("Name") add_text("John Stuart")
-@> sc new_child("Description") add_text("John Stuart RSI GraphBuilder.jl test file")
-ip = @> "InitialParameters" new_element
-@> ip new_child("PatientFile") add_text("JohnStuart.xml")
-add_child(sc, ip)
-drqs = @> "DataRequests" new_element
-set_attribute(drqs, "Filename", "../JohnStuartRSI-072016/data/orig/001.csv")
-drq = @> "DataRequest" new_element
-set_attributes(drq, drq_attr_hr)
-add_child(drqs, drq)
-drq = @> "DataRequest" new_element
-set_attributes(drq, drq_attr_map)
-add_child(drqs, drq)
-drq = @> "DataRequest" new_element
-set_attributes(drq, drq_attr_spo2)
-add_child(drqs, drq)
-add_child(sc, drqs)
-act = @> "Action" ne
+
+sc = @> "Scenario" ne
+@> sc set_attributes(scenario_attr)
+@> x set_root(sc)
+
+@> sc chldtxt("Name", "John Stuart")
+@> sc chldtxt("Description", "John Stuart RSI GraphBuilder.jl test file")
+
+ip = @> "InitialParameters" ne
+@> ip chldtxt("PatientFile", "JohnStuart.xml")
+@> sc add_child(ip)
+
+drqs = @> "DataRequests" ne
+@> drqs set_attribute("Filename", "../JohnStuartRSI-072016/data/orig/001.csv")
+
+drq = @>> data_requests map(x -> (x, ne("DataRequest")))
+@>> drq foreach(x -> set_attributes(x[2], x[1]))
+@>> drq foreach(x -> add_child(drqs, x[2]))
+
+@> sc add_child(drqs)
+
+act = @> "Action" ne @> set_attribute("xsi:type", "AdvanceTimeData")
 @> act set_attribute("xsi:type", "AdvanceTimeData")
+
 c = @> "Comment" ne
 @> c add_text("w00t! w00t! w00t! w00t! w00t! w00t! w00t! w00t! w00t!")
 add_child(act, c)
+
 t = @> "Time" ne
 @> t set_attributes(["value" => "99", "unit" => "s"])
 add_child(act, t)
