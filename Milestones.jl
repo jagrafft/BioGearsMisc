c9fb2b561d15917be8896e7ccf36f4881e9ab7c6
jason a. grafft <graf0096@umn.edu>
2019-03-09 07:54:19 -0600

    Added to Milestones.jl

--- Milestones.jl
+++ Milestones.jl
@@ -1,26 +1,33 @@
-using CSV, DataFrames, JuliaDB, OnlineStats, Plots, StatsBase, StatsPlots, GR; gr(dpi=400)
+using CSV, DataFrames, JuliaDB, HypothesisTests, OnlineStats, Plots, StatsBase, StatsPlots, GR; gr(dpi=400)
 
-gi = loadtable("data/Milestones/2017-2018/GIBleed-num.csv");
-ob = loadtable("data/Milestones/2017-2018/OBD-num.csv");
+gi = loadtable("../data/Milestones/2017-2018/GIBleed-num.csv");
+ob = loadtable("../data/Milestones/2017-2018/OBD-num.csv");
 
 sample_size = 375
 # samplings = 15
 
 # Statistics
 ## Descriptive
-gi_desc = map(x -> NamedTuple{(:id,:len,:min,:max,:μ,:σ,:cov,:skewness,:kurtosis)}(tuple(x[2], round.([length(x[1]), minimum(x[1]), maximum(x[1]), mean(x[1]), std(x[1]), cov(x[1]), skewness(x[1]), kurtosis(x[1])], digits=2)...)), columns(select(gi, Between(8,38))) |> x -> zip(values(x), keys(x))) |> table
+gi_desc = map(x -> NamedTuple{(:id,:len,:min,:max,:μ,:σ,:cov,:skewness,:kurtosis)}(tuple(x[2], round.([length(x[1]), minimum(x[1]), maximum(x[1]), mean(x[1]), std(x[1]), OnlineStats.cov(x[1]), skewness(x[1]), kurtosis(x[1])], digits=2)...)), columns(select(gi, Between(8,38))) |> x -> zip(values(x), keys(x))) |> table
 
-ob_desc = map(x -> NamedTuple{(:id,:len,:min,:max,:μ,:σ,:cov,:skewness,:kurtosis)}(tuple(x[2], round.([length(x[1]), minimum(x[1]), maximum(x[1]), mean(x[1]), std(x[1]), cov(x[1]), skewness(x[1]), kurtosis(x[1])], digits=2)...)), columns(select(ob, Between(8,39))) |> x -> zip(values(x), keys(x))) |> table
+ob_desc = map(x -> NamedTuple{(:id,:len,:min,:max,:μ,:σ,:cov,:skewness,:kurtosis)}(tuple(x[2], round.([length(x[1]), minimum(x[1]), maximum(x[1]), mean(x[1]), std(x[1]), OnlineStats.cov(x[1]), skewness(x[1]), kurtosis(x[1])], digits=2)...)), columns(select(ob, Between(8,39))) |> x -> zip(values(x), keys(x))) |> table
+
+## Unequal variance T-tests
+gi_ost = ((reduce((a,c) -> (push!(a[1], round.([v for v in map(x -> UnequalVarianceTTest(select(gi, c), select(gi, x)) |> pvalue, a[2])], digits=2)); a[2] = a[2][2:end]; a), colnames(gi)[8:38], init=[[], colnames(gi)[8:38]])[1] .|> x -> vcat([Missing for z in 1:1:(31-length(x))], x)) |> m -> zip(colnames(gi)[8:38], m) .|> x -> NamedTuple{tuple(:features, colnames(gi)[8:38]...)}(tuple([x[1], x[2]...]...))) |> table
+
+ob_ost = ((reduce((a,c) -> (push!(a[1], round.([v for v in map(x -> UnequalVarianceTTest(select(ob, c), select(ob, x)) |> pvalue, a[2])], digits=2)); a[2] = a[2][2:end]; a), colnames(ob)[8:39], init=[[], colnames(ob)[8:39]])[1] .|> x -> vcat([Missing for z in 1:1:(32-length(x))], x)) |> m -> zip(colnames(ob)[8:39], m) .|> x -> NamedTuple{tuple(:features, colnames(gi)[8:39]...)}(tuple([x[1], x[2]...]...))) |> table
+
+feat_ost = [x for x in reduce((a,c) -> (push!(a[1], (i=a[2], feature=c, pval=(([gi, ob] .|> x -> select(x, c)) |> x -> round(UnequalVarianceTTest(x[1], x[2]) |> pvalue, digits=2)))); a[2] = a[2] + 1; a), intersect(colnames(gi), colnames(ob))[8:34], init=[[], 1])[1]] |> table
 
 ## Covarience
-gi_cov = ((reduce((a,c) -> (push!(a[1], round.([v for v in map(x -> cov(select(gi, c), select(gi, x)), a[2])], digits=2)); a[2] = a[2][2:end]; a), colnames(gi)[8:38], init=[[], colnames(gi)[8:38]])[1] .|> x -> vcat([Missing for z in 1:1:(31-length(x))], x)) |> m -> zip(colnames(gi)[8:38], m) .|> x -> NamedTuple{tuple(:features, colnames(gi)[8:38]...)}(tuple([x[1], x[2]...]...))) |> table
+gi_cov = ((reduce((a,c) -> (push!(a[1], round.([v for v in map(x -> OnlineStats.cov(select(gi, c), select(gi, x)), a[2])], digits=2)); a[2] = a[2][2:end]; a), colnames(gi)[8:38], init=[[], colnames(gi)[8:38]])[1] .|> x -> vcat([Missing for z in 1:1:(31-length(x))], x)) |> m -> zip(colnames(gi)[8:38], m) .|> x -> NamedTuple{tuple(:features, colnames(gi)[8:38]...)}(tuple([x[1], x[2]...]...))) |> table
 
-ob_cov = ((reduce((a,c) -> (push!(a[1], round.([v for v in map(x -> cov(select(ob, c), select(ob, x)), a[2])], digits=2)); a[2] = a[2][2:end]; a), colnames(ob)[8:39], init=[[], colnames(ob)[8:39]])[1] .|> x -> vcat([Missing for z in 1:1:(32-length(x))], x)) |> m -> zip(colnames(ob)[8:39], m) .|> x -> NamedTuple{tuple(:features, colnames(gi)[8:39]...)}(tuple([x[1], x[2]...]...))) |> table
+ob_cov = ((reduce((a,c) -> (push!(a[1], round.([v for v in map(x -> OnlineStats.cov(select(ob, c), select(ob, x)), a[2])], digits=2)); a[2] = a[2][2:end]; a), colnames(ob)[8:39], init=[[], colnames(ob)[8:39]])[1] .|> x -> vcat([Missing for z in 1:1:(32-length(x))], x)) |> m -> zip(colnames(ob)[8:39], m) .|> x -> NamedTuple{tuple(:features, colnames(gi)[8:39]...)}(tuple([x[1], x[2]...]...))) |> table
 
-feat_cov = [x for x in reduce((a,c) -> (push!(a[1], (i=a[2], N=sample_size, feature=c, covariance=([gi, ob] .|> x -> select(x, :introduce_self) |> x -> StatsBase.sample(x, sample_size, replace=false)) |> x -> round(cov(x[1], x[2]), digits=2))); a[2] = a[2] + 1; a), intersect(colnames(gi), colnames(ob))[8:34], init=[[], 1])[1]] |> table
+feat_cov = [x for x in reduce((a,c) -> (push!(a[1], (i=a[2], N=sample_size, feature=c, cov=([gi, ob] .|> x -> select(x, c) |> x -> StatsBase.sample(x, sample_size, replace=false)) |> x -> round(OnlineStats.cov(x[1], x[2]), digits=2))); a[2] = a[2] + 1; a), intersect(colnames(gi), colnames(ob))[8:34], init=[[], 1])[1]] |> table
 
-## 
-[("DescriptiveStats_OBD.csv", ob_desc), ("DescriptiveStats_GIBleed.csv", gi_desc), ("Covariance_GIBleed.csv", gi_cov), ("Covariance_OBD.csv", ob_cov), ("Covariance_by_common_feature.csv", feat_cov)] .|> x -> CSV.write(x...)
+## Write CSV
+[("DescriptiveStats_GIBleed.csv", gi_desc), ("DescriptiveStats_OBD.csv", ob_desc), ("UneqVarTTest_GIBleed.csv", gi_ost), ("UneqVarTTest_OBD.csv", ob_ost), ("UneqVarTTest_by_common_feature.csv", feat_ost), ("Covariance_GIBleed.csv", gi_cov), ("Covariance_OBD.csv", ob_cov), ("Covariance_by_common_feature.csv", feat_cov)] .|> x -> CSV.write(x...)
 
 # Plots
 gi_feats_names = [String(x) for x in colnames(select(gi, Between(8,38)))]
